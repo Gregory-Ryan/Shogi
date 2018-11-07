ab347764a2e6d381e91d75fb71031d52ee8eaacf
Gregory-Ryan <44442147+Gregory-Ryan@users.noreply.github.com>
2018-11-07 16:35:11 -0500

    Update cf.py

--- cf.py
+++ cf.py
@@ -596,3 +596,22 @@ def L(k,s,X1) :
         if mod(subt(z4,multi(0.00000000999,z4))) <= mod(z1) <= mod(add(z4,multi(0.00000000999,z4)))   :
             return z4
     return math.inf
+
+def H_n(n) :
+    #Calculates the Harmonic Series the nth term
+    z1 = 0
+    for i in range(1, n + 1) :
+        z2 = 1 / i
+        z1 = z1 + z2
+    return z1
+    
+def polygamma(n) :
+    #Calculates the polygamma function (Only real int)
+    z1 = 1 - math.log(2)
+    for i in range(2,1000000) :
+        z2 = 1 / i - math.log(1 + (1 / i))
+        z1 = z1 + z2
+    z3 = H_n(n - 1) - z1
+    return z3
+    
+euler_c = - polygamma(1)
