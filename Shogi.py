000f20e84fc3ec808cd2260f63425ea79857e802
Gregory.Ryan <gregory.ryan@echs.ca>
2018-12-05 17:08:40 -0500

    changes

---
+++ Shogi.py
@@ -0,0 +1,26 @@
+import turtle
+
+
+def move(x,y):
+    selected = turn_order[len(turn_order)]
+
+
+def select(x,y):
+    selected = []
+    if turn_counter % 2 != 0:
+        for piece in player_one:
+            if piece[2] == x and piece[3] == y:
+                selected = piece
+    elif turn_counter % 2 == 0:
+        for piece in player_two:
+            if piece[2] == x and piece[3] == y:
+                selected = piece
+    turn_order.appened(selected)
+
+
+player_one = []
+player_two = []
+turn_counter = 1
+turn_order = []
+
+wn = turtle.Screen()
