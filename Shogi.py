638c561a80d65f7f19eb251ab4315c91fc4dfc5a
Gregory.Ryan <gregory.ryan@echs.ca>
2018-12-06 15:59:24 -0500

    changes

--- Shogi.py
+++ Shogi.py
@@ -1,46 +1,45 @@
 import turtle
 
-
 def death(piece,player):
     if player == 1:
         player_one.remove(piece)
         player_one_dead.append(piece)
-        turtle_name = piece[1]
+        turtle_name = piece[0]
         turtle_name.turn(180)
     elif player == 2:
         player_two.remove(piece)
         player_two_dead.append(piece)
-        turtle_name = piece[1]
+        turtle_name = piece[0]
         turtle_name.turn(180)
 
 
 def move(x,y):
     fail = 0
-    selected = turn_order[len(turn_order)]
-    turtle_name = selected[1]
-    if turn_counter % 2 != 0:
+    selected = turn_order[len(turn_order) -1]
+    turtle_name = selected[0]
+    if len(turn_order) + 1 % 2 != 0:
         if selected in player_two_dead:
             player_two_dead.remove(selected)
             player_one.append(selected)
         for piece in player_one:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] == x and piece[2] == y:
                 fail = 1
         for piece in player_two:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] == x and piece[2] == y:
                 death(piece,2)
-    elif turn_counter % 2 == 0:
+    elif len(turn_order) + 1 % 2 == 0:
         if selected in player_one_dead:
             player_one_dead.remove(selected)
             player_two.append(selected)
         for piece in player_two:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] == x and piece[2] == y:
                 fail = 1
         for piece in player_one:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] == x and piece[2] == y:
                 death(piece,1)
     if fail != 1:
         turtle_name.setpos(x,y)
-        turn_counter += 1
+#        turn_counter += 1
         wn.onclick(select)
 
     else:
@@ -49,19 +48,19 @@ def move(x,y):
 
 def select(x,y):
     selected = []
-    if turn_counter % 2 != 0:
+    if len(turn_order) + 1 % 2 != 0:
         for piece in player_one:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] - 50 <= x <= piece[1] + 50 and piece[2] - 50 <= y <= piece[2] + 50:
                 selected = piece
         for piece in player_two_dead:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] - 50 <= x <= piece[1] + 50 and piece[2] - 50 <= y <= piece[2] + 50:
                 selected = piece
-    elif turn_counter % 2 == 0:
+    elif len(turn_order) + 1 % 2 == 0:
         for piece in player_two:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] - 50 <= x <= piece[1] + 50 and piece[2] - 50 <= y <= piece[2] + 50:
                 selected = piece
         for piece in player_one_dead:
-            if piece[2] == x and piece[3] == y:
+            if piece[1] - 50 <= x <= piece[1] + 50 and piece[2] - 50 <= y <= piece[2] + 50:
                 selected = piece
     if selected != []:
         turn_order.append(selected)
@@ -72,24 +71,24 @@ def select(x,y):
 
 player_one = [['turtle1',10,100]]
 player_one_dead = []
-player_two = [['turtle2',10,100]]
+player_two = [['turtle2',200,100]]
 player_two_dead = []
-turn_counter = 1
+# turn_counter = 1
 turn_order = []
 
 wn = turtle.Screen()
 
 for player_one_set in player_one:
-    turtle_name_set = player_one_set[1]
+    turtle_name_set = player_one_set[0]
     turtle_name_set = turtle.Turtle()
-    turtle_name_set.setpos(player_one_set[2],player_one_set[3])
+    turtle_name_set.setpos(player_one_set[1],player_one_set[2])
 
 for player_two_set in player_two:
-    turtle_name_set = player_two_set[1]
+    turtle_name_set = player_two_set[0]
     turtle_name_set = turtle.Turtle()
-    turtle_name_set.setpos(player_two_set[2],player_two_set[3])
+    turtle_name_set.setpos(player_two_set[1],player_two_set[2])
 
-if turn_counter == 1:
+if len(turn_order) + 1 == 1:
     wn.onclick(select)
 
 
