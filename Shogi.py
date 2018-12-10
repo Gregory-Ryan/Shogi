a97f39a92de530bdd89b85cdcf091ef8b2d0435d
Gregory.Ryan <gregory.ryan@echs.ca>
2018-12-10 11:24:01 -0500

    new things

--- Shogi.py
+++ Shogi.py
@@ -6,21 +6,21 @@ def make_yards():
     make.ht()
     make.speed(0)
     make.penup()
-    make.setpos(-400,-175)
+    make.setpos(-400, -175)
     make.pendown()
     make.forward(150)
     make.back(150)
     make.left(180)
-    for q in range(0,2):
+    for q in range(0, 2):
         if q == 1:
             make.penup()
-            make.setpos(400,175)
+            make.setpos(400, 175)
             make.pendown()
             make.left(90)
             make.forward(150)
             make.back(150)
             make.right(180)
-        for i in range(0,7):
+        for i in range(0, 7):
             if i % 2 == 0:
                 make.right(90)
                 make.forward(50)
@@ -48,23 +48,18 @@ def make_yards():
         make.forward(350)
 
 
-
-
-
-
-
-def round_to_mid(x,y):
+def round_to_mid(x, y):
     for location in middle:
         if location[0] - 25 <= x <= location[0] + 25 and location[1] - 25 <= y <= location[1] + 25:
-            return [location[0],location[1]]
+            return [location[0], location[1]]
 
 
 def mid():
-    mid = []
-    for mid_y in range(0,9):
-        for mid_x in range(0,9):
-            mid.append([-200 + mid_x * 50, 200 - mid_y * 50])
-    return mid
+    midd = []
+    for midd_y in range(0, 9):
+        for midd_x in range(0, 9):
+            midd.append([-200 + midd_x * 50, 200 - midd_y * 50])
+    return midd
 
 
 def make_board():
@@ -72,11 +67,11 @@ def make_board():
     maker.ht()
     maker.speed(0)
     maker.penup()
-    maker.setpos(225,-225)
+    maker.setpos(225, -225)
     maker.pendown()
-    for lines in range(0,2):
+    for lines in range(0, 2):
         maker.left(90)
-        for repeat in range(0,5):
+        for repeat in range(0, 5):
             maker.forward(450)
             maker.left(90)
             maker.forward(50)
@@ -88,61 +83,86 @@ def make_board():
                 maker.right(90)
 
 
-def death(piece,player):
+def death(piece, player):
     if player == 1:
         player_one.remove(piece)
         player_one_dead.append(piece)
         turtle_name = piece[3]
-        turtle_name.turn(180)
+        turtle_name.right(180)
+        turtle_name.clear()
+        turtle_name.setpos(375 - 50 * (len(player_one_dead) - 1), 150)
+        turtle_name.write(piece[0], align="center")
+        del piece[1]
+        piece.insert(1, 375 - 50 * (len(player_one_dead) - 1))
+        del piece[2]
+        piece.insert(2, 150)
     elif player == 2:
         player_two.remove(piece)
         player_two_dead.append(piece)
         turtle_name = piece[3]
-        turtle_name.turn(180)
+        turtle_name.right(180)
+        turtle_name.clear()
+        turtle_name.setpos(-375 + 50 * (len(player_two_dead) - 1), 150)
+        turtle_name.write(piece[0], align="center")
+        del piece[1]
+        piece.insert(1, -375 + 50 * (len(player_two_dead) - 1))
+        del piece[2]
+        piece.insert(2, 150)
 
 
-def move(u,v):
+def move(u, v):
     global turn_counter
-    cord = round_to_mid(u,v)
+    cord = round_to_mid(u, v)
     x = cord[0]
     y = cord[1]
     fail = 0
-    selected = turn_order[len(turn_order) -1]
+    returned = 0
+    selected = turn_order[len(turn_order) - 1]
     turtle_name = selected[3]
     if turn_counter % 2 != 0:
         if selected in player_two_dead:
             player_two_dead.remove(selected)
             player_one.append(selected)
+            returned = 1
         for piece in player_one:
             if piece[1] == x and piece[2] == y:
                 fail = 1
         for piece in player_two:
             if piece[1] == x and piece[2] == y:
-                death(piece,2)
+                if returned == 1:
+                    fail = 1
+                else:
+                    death(piece, 2)
     elif turn_counter % 2 == 0:
         if selected in player_one_dead:
             player_one_dead.remove(selected)
             player_two.append(selected)
+            returned = 1
         for piece in player_two:
             if piece[1] == x and piece[2] == y:
                 fail = 1
         for piece in player_one:
             if piece[1] == x and piece[2] == y:
-                death(piece,1)
+                if returned == 1:
+                    fail = 1
+                else:
+                    death(piece, 1)
     if fail != 1:
         del selected[1]
-        selected.insert(1,x)
+        selected.insert(1, x)
         del selected[2]
-        selected.insert(2,y)
-        turtle_name.setpos(x,y)
+        selected.insert(2, y)
+        turtle_name.clear()
+        turtle_name.setpos(x, y)
         turtle_name.color("black")
+        turtle_name.write(selected[0], align="center")
         turn_counter += 1
         wn.onclick(select)
     else:
         wn.onclick(move)
 
 
-def select(x,y):
+def select(x, y):
     global turn_counter
     selected = []
     fail = 1
@@ -173,9 +193,9 @@ def select(x,y):
         wn.onclick(select)
 
 
-player_one = [['turtle1',10,100,turtle.Turtle()]]
+player_one = [['turtle1', 50, 100, turtle.Turtle()], ['turtle3', 100, 100, turtle.Turtle()]]
 player_one_dead = []
-player_two = [['turtle2',200,100,turtle.Turtle()]]
+player_two = [['turtle2', 200, 100, turtle.Turtle()], ['turtle4', 200, 150, turtle.Turtle()]]
 player_two_dead = []
 turn_order = []
 middle = mid()
@@ -189,17 +209,17 @@ make_yards()
 for player_one_set in player_one:
     turtle_names = player_one_set[3]
     turtle_names.penup()
-    turtle_names.setpos(player_one_set[1],player_one_set[2])
+    turtle_names.setpos(player_one_set[1], player_one_set[2])
+    turtle_names.write(player_one_set[0], align="center")
 
 for player_two_set in player_two:
     turtle_names = player_two_set[3]
     turtle_names.penup()
-    turtle_names.setpos(player_two_set[1],player_two_set[2])
+    turtle_names.setpos(player_two_set[1], player_two_set[2])
     turtle_names.right(180)
-
+    turtle_names.write(player_two_set[0], align="center")
 
 if turn_counter == 1:
     wn.onclick(select)
-
 
 wn.mainloop()
